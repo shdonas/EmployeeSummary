0967ac1f51722330c92866c467faef345c251c21
shdonas <BlackHole7473>
2020-05-05 16:29:35 -0400

    first commit

--- Develop/app.js
+++ Develop/app.js
@@ -119,7 +119,6 @@ function EngineerQuestions() {
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
-render;
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
